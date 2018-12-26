f4342a40e861f878e2d29f9234a42ef8b337d615
Yang Minghui <minghuiyang1998@gmail.com>
2018-12-26 11:19:03 +0800

    [*]

--- pages/login.vue
+++ pages/login.vue
@@ -3,7 +3,7 @@
     class="d-flex flex-justify-center flex-items-center"
     style="background: linear-gradient(-60deg,#fff 40%,  #57B08F 40%) ; height: 100vh;"
   >
-    <div class="col-md-5 col-lg-5 rounded-1 text-gray bg-white py-4 px-4 border box-shadow-large">
+    <div class="position-relative col-md-5 col-lg-5 rounded-1 text-gray bg-white py-6 px-4 border box-shadow-large">
       <!-- <div style="width:200px;height:200px;" class="mx-auto">
           <lottie
             :options="defaultOptions"
@@ -12,8 +12,8 @@
             v-on:animCreated="handleAnimation"
           />
       </div>-->
-      <div ref="lavContainer" style="width: 200px; height: 200px;"></div>
-      <el-form :model="user">
+      <div ref="lavContainer" class="position-absolute left-0 right-0 mx-auto " style="width: 200px; height: 200px; top:-100px;"></div>
+      <el-form :model="user" class="mt-6">
         <el-form-item prop="name" required="true">
           <label class="form-label f5">username</label>
           <el-input type="username" v-model="user.name" autocomplete="off"></el-input>
