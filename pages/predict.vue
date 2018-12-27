cfd029effd5705a6addcf308bb07a2444ad1453e
Yang Minghui <minghuiyang1998@gmail.com>
2018-12-27 15:57:00 +0800

    [+]new predict except style

--- pages/predict.vue
+++ pages/predict.vue
@@ -3,8 +3,8 @@
     class="d-flex flex-justify-center flex-items-center"
     style="background: linear-gradient(-60deg,#fff 40%,  #57B08F 40%) ; height: 100vh;"
   >
-    <div class="col-md-8 col-lg-8 text-gray bg-white py-4 px-4 rounded-2 border box-shadow-large">
-      <el-steps :active="step" finish-status="success" align-center>
+    <div class="col-md-8 col-lg-8 text-gray bg-white py-4 rounded-2 border box-shadow-large">
+      <el-steps :active="step" align-center>
         <el-step title="选择联赛" description></el-step>
         <el-step title="选择参赛队" description></el-step>
         <el-step title="选择参赛队" description></el-step>
@@ -12,73 +12,77 @@
         <el-step title="预测" description></el-step>
       </el-steps>
 
-      <div v-if="currentTable === 'league'">
-        <el-table
-          ref="league"
-          :data="leagueData.leagues"
-          @row-click="onLeagueSelect"
-          highlight-current-row
-          style="width: 100%"
-        >
-          <el-table-column align="center" property="name" label="联赛名称" row-key="lid"></el-table-column>
-        </el-table>
+      <div v-show="currentTable === 'league'">
+        <div class="text-center mt-2 f4">联赛名称</div>
+        <div class="overflow-auto border-top" style="height: 200px;">
+          <div
+            v-for="league in leagues"
+            @click="onLeagueSelect(league.lid)"
+            class="table-item border text-center py-3 mt-2"
+            :class="league.lid == current.league ? 'selected':''"
+            :key="league.lid"
+          >{{league.name}}</div>
+        </div>
         <div class="d-flex mt-4 flex-justify-between flex-items-center">
           <el-button disabled>上一步</el-button>
           <el-button @click="onNextButtonClick()">下一步</el-button>
         </div>
       </div>
 
-      <div v-else-if="currentTable === 'teamA'">
-        <el-table
-          ref="teamA"
-          :data="teamData.teams"
-          @row-click="onTeamASelect"
-          highlight-current-row
-          style="width: 100%"
-        >
-          <el-table-column align="center" property="name" label="球队名称" row-key="tid"></el-table-column>
-        </el-table>
+      <div v-show="currentTable === 'teamA'">
+        <div class="text-center mt-2 f4">球队名称</div>
+        <div class="overflow-auto border-top" style="height: 200px;">
+          <div
+            v-for="team in teams"
+            @click="onTeamASelect(team.tid)"
+            class="table-item border text-center py-3 mt-2"
+            :class="team.tid == current.teamA ? 'selected':''"
+            :key="team.tid"
+          >{{team.name}}</div>
+        </div>
         <div class="d-flex mt-4 flex-justify-between flex-items-center">
           <el-button @click="onBackButtonClick()">上一步</el-button>
           <el-button @click="onNextButtonClick()">下一步</el-button>
         </div>
       </div>
 
-      <div v-else-if="currentTable === 'teamB'">
-        <el-table
-          ref="teamB"
-          :data="teamData.teams"
-          highlight-current-row
-          @row-click="onTeamBSelect"
-          style="width: 100%"
-        >
-          <el-table-column align="center" property="name" label="球队名称" row-key="tid"></el-table-column>
-        </el-table>
+      <div v-show="currentTable === 'teamB'">
+        <div class="text-center mt-2 f4">球队名称</div>
+        <div class="overflow-auto border-top" style="height: 200px;">
+          <div
+            v-for="team in teams"
+            @click="onTeamBSelect(team.tid)"
+            class="table-item border text-center py-3 mt-2"
+            :class="team.tid == current.teamA ? 'another':(team.tid == current.teamB ? 'selected' : '')"
+            :key="team.tid"
+          >{{team.name}}</div>
+        </div>
         <div class="d-flex mt-4 flex-justify-between flex-items-center">
           <el-button @click="onBackButtonClick()">上一步</el-button>
           <el-button @click="onNextButtonClick()">下一步</el-button>
         </div>
       </div>
 
-      <div v-else-if="currentTable === 'rule'">
-        <el-table
-          ref="rule"
-          :data="ruleData.rules"
-          highlight-current-row
-          @row-click="onRuleSelect"
-          style="width: 100%"
-        >
-          <el-table-column align="center" property="rule" label="赛制" row-key="rid"></el-table-column>
-        </el-table>
+      <div v-show="currentTable === 'rule'">
+        <div class="text-center mt-2 f4">赛制</div>
+        <div class="overflow-auto border-top" style="height: 200px;">
+          <div
+            v-for="rule in rules"
+            @click="onRuleSelect(rule.rid)"
+            class="table-item border text-center py-3 mt-2"
+            :class="rule.rid == current.rule ? 'selected':''"
+            :key="rule.rid"
+          >{{rule.rule}}</div>
+        </div>
         <div class="d-flex mt-4 flex-justify-between flex-items-center">
           <el-button @click="onBackButtonClick()">上一步</el-button>
           <el-button @click="onPredictButtonClick()">开始预测</el-button>
         </div>
       </div>
 
-      <div v-else-if="currentTable === 'predict'">
-        <div v-if="isLoading" ref="lavContainer" style="width: 200px; height: 200px;"></div>
-        <div v-else class="d-flex m-6 flex-items-center">
+      <div v-show="currentTable === 'predict'" class="position-relative">
+        <div v-show="isLoading" ref="loading" class="mx-auto my-8 bg-white" style="width: 200px; height: 200px;"></div>
+        <div v-if="!isLoading" class="d-flex m-6 flex-items-center">
           <div class="col-3 text-center">
             <img v-bind:src="result.teamA.avatar" class="img circle mr-2">
             <div class="h1">{{result.teamA.name}}</div>
@@ -109,87 +113,75 @@ import lottie from "lottie-web";
 
 export default {
   async asyncData({ $axios }) {
-    let leagueData = {
-      default: 1,
-      leagues: [
-        {
-          lid: 1,
-          name: "league-name"
-        },
-        {
-          lid: 2,
-          name: "league-name"
-        },
-        {
-          lid: 3,
-          name: "league-name"
-        },
-        {
-          lid: 4,
-          name: "league-name"
-        }
-      ]
-    };
+    let leagues = [
+      {
+        lid: 1,
+        name: "league-name"
+      },
+      {
+        lid: 2,
+        name: "league-name"
+      },
+      {
+        lid: 3,
+        name: "league-name"
+      },
+      {
+        lid: 4,
+        name: "league-name"
+      }
+    ];
 
-    let teamData = {
-      default: 1,
-      teams: [
-        {
-          tid: 1,
-          name: "team-name_A"
-        },
-        {
-          tid: 2,
-          name: "team-name_A"
-        },
-        {
-          tid: 3,
-          name: "team-name_A"
-        },
-        {
-          tid: 4,
-          name: "team-name_A"
-        }
-      ]
-    };
+    let teams = [
+      {
+        tid: 1,
+        name: "team-name_A"
+      },
+      {
+        tid: 2,
+        name: "team-name_A"
+      },
+      {
+        tid: 3,
+        name: "team-name_A"
+      },
+      {
+        tid: 4,
+        name: "team-name_A"
+      }
+    ];
 
-    let ruleData = {
-      default: 1,
-      rules: [
-        {
-          rid: 1,
-          rule: "1 vs 1"
-        },
-        {
-          rid: 2,
-          rule: "1 vs 1"
-        },
-        {
-          rid: 3,
-          rule: "1 vs 1"
-        },
-        {
-          rid: 4,
-          rule: "1 vs 1"
-        },
-        {
-          rid: 5,
-          rule: "1 vs 1"
-        },
-        {
-          rid: 6,
-          rule: "1 vs 1"
-        }
-      ]
-    };
+    let rules = [
+      {
+        rid: 1,
+        rule: "1 vs 1"
+      },
+      {
+        rid: 2,
+        rule: "1 vs 1"
+      },
+      {
+        rid: 3,
+        rule: "1 vs 1"
+      },
+      {
+        rid: 4,
+        rule: "1 vs 1"
+      },
+      {
+        rid: 5,
+        rule: "1 vs 1"
+      },
+      {
+        rid: 6,
+        rule: "1 vs 1"
+      }
+    ];
+
     return {
-      leagueData: leagueData,
-      teamData: teamData,
-      ruleData: ruleData,
-      [`current.league`]:leagueData.default,
-      [`current.teamA`]:teamData.default,
-      [`current.teamB`]:teamData.default,
-      [`current.rule`]:ruleData.default,
+      leagues: leagues,
+      teams: teams,
+      rules: rules
     };
   },
   data() {
@@ -198,36 +190,27 @@ export default {
       isLoading: true,
       result: {
         teamA: {
-          name:'',
-          avatar:''
+          name: "",
+          avatar: ""
         },
         teamB: {
-          name:'',
-          avatar:''
+          name: "",
+          avatar: ""
         },
         winRate: "",
         loseRate: "",
         rate: ""
       },
 
-      leagueData: {
-        default: 0,
-        leagues: []
-      },
-      teamData: {
-        default: 0,
-        teams: []
-      },
-      ruleData: {
-        default: 0,
-        rules: []
-      },
+      leagues: [],
+      teams: [],
+      rules: [],
 
       current: {
         league: 1,
-        teamA: 1,
-        teamB: 1,
-        rule: 1
+        teamA: 2,
+        teamB: 3,
+        rule: 4
       },
 
       anim: {},
@@ -236,7 +219,7 @@ export default {
   },
   mounted() {
     this.anim = lottie.loadAnimation({
-      container: this.$refs.lavContainer,
+      container: this.$refs.loading,
       renderer: "svg",
       loop: true,
       autoplay: true,
@@ -260,41 +243,61 @@ export default {
           rate: 90
         };
         this.isLoading = false;
-      }, 500);
+      }, 5000);
+    },
+    async _getTeamData() {
+      //获取team
+      try {
+
+      } catch(e) {
+        console.log(e)
+      }
+    },
+    async _getRuleData() {
+      //获取rule
+      try {
+
+      } catch(e) {
+        console.log(e)
+      }
     },
     _changeCurrentTable(step) {
       let index = tableStatesENUM.indexOf(this.currentTable);
-      let table = tableStatesENUM[index + step];
-
-      this.currentTable = table;
-      console.log(this.current[table]);
-      this.$refs[table].setCurrentRow();
+      //即将要换上的表格
+      let oldTable = tableStatesENUM[index];
+      let newTable = tableStatesENUM[index + step];
+      if (oldTable === "league" && newTable === "teamA") {
+        // 如果是team就要重新拉一次数据,因为league可能会不一样
+        this._getTeamData()
+      } else if (oldTable === "teamB" && newTable === "rule") {
+        // 如果是rule也要重新拉一次数据，因为league可能会不一样
+        this._getRuleData()
+      } else {
+      }
+      this.currentTable = newTable;
     },
     onLeagueSelect(val) {
       console.log(val);
       this.current.league = val;
-      this.step = 1;
     },
     onTeamASelect(val) {
       console.log(val);
       this.current.teamA = val;
-      this.step = 2;
     },
     onTeamBSelect(val) {
       console.log(val);
       this.current.teamB = val;
-      this.step = 3;
     },
     onRuleSelect(val) {
       console.log(val);
       this.current.rule = val;
-      this.step = 4;
     },
     onBackButtonClick() {
       this.step -= 1;
       this._changeCurrentTable(-1);
     },
     onNextButtonClick() {
+      this.step += 1;
       this._changeCurrentTable(1);
     },
     onPredictButtonClick() {
@@ -310,5 +313,20 @@ export default {
 .img {
   width: 100px;
   height: 100px;
+}
+
+.table-item:hover {
+  box-shadow: 0 1px 15px rgba(27, 31, 35, 0.15);
+  background-color: rgba(27, 31, 35, 0.15);
+}
+
+.selected {
+  box-shadow: 0 1px 15px rgba(27, 31, 35, 0.15);
+  background-color: rgba(27, 31, 35, 0.15);
+}
+
+.another {
+  box-shadow: 0 1px 15px rgba(27, 31, 35, 0.15);
+  background-color: red;
 }
 </style>
